ea4e56822cebce0bec6ad5351b0db83b79191dcf
Yenkis <jesusdbarajas@gmail.com>
2019-01-03 11:56:08 -0500

    images up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,14 +25,14 @@ export class AppComponent {
       new Product(
         'NICEHAT',
         'Leather Jacket',
-        '../assets/images/products/brow-jacket.jpg',
+        'https://raw.githubusercontent.com/Jesusd19/InventarioApp/master/src/assets/images/products/brow-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'],
         267.99
       ),
       new Product(
         'NICEHAT',
         'Cistera Top Gray Hat',
-        '../assets/images/products/gray-hat.jpg',
+        'https://raw.githubusercontent.com/Jesusd19/InventarioApp/master/src/assets/images/products/gray-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         49.99
       ),
